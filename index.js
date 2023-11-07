024b44d1cf66660750ec62ddd1d51baebac922f0
Quachell Peele <quachellpeele@gmail.com>
2023-11-06 19:50:00 -0500

    i finished this

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random()+4);
+
+let num3 = 18
+let num4 = 7
+let mod = num3 % num4
+
+
+let max = Math.max(10,5,20,15);
+
+console.log(max);
